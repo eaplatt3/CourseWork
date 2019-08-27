e097c240d8daa056474842ca93d1de6837c3a7d0
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-08-27 11:35:53 -0400

    Init Commit

---
+++ CourseWorkLibrary/CourseWorkLibrary/CourseWorkLibaray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+//**********************************************************//
+// File: CourseWorkLibaray.cs                               //
+//                                                          //
+// Purpose: Contains the class definitions for CourseWork.  //
+//          These Class will be used as test classes.       //
+//                                                          //
+// Written By: Earl Platt III                               //
+//                                                          //
+// Compiler: Visual Studio 2019                             //
+//                                                          //
+//**********************************************************//
+
+namespace CourseWorkLibrary
+{
+    
+    public class Category{
+
+        //Member Variables
+        private String name;
+        private Double percentage;
+
+        //Constructor
+        public Category()
+        {
+
+        }
+
+    }
+}
