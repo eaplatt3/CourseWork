1c9e0ee606a547e44f4c874bb6d67505ed9ceb74
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-08-27 15:11:44 -0400

    Expaned Menu

--- CourseWork/CourseWork/Program.cs
+++ CourseWork/CourseWork/Program.cs
@@ -19,17 +19,45 @@ namespace CourseWorkLibrary {
                 {
                     Console.WriteLine("CourseWork Testing Menu");
                     Console.WriteLine("-----------------------");
-                    Console.WriteLine("1 - Unit Test Catgory");
+                    Console.WriteLine("1 - Unit Test Category");
                     Console.WriteLine("2 - Unit Test Assigment");
                     Console.WriteLine("3 - Exit");
                     Console.Write("Enter Choice: ");
                     num = Console.ReadLine();
+                    Console.WriteLine("");
 
                     if (!Int32.TryParse(num, out i))
                     {
 
                     }
                     
+                    if(i == 1)
+                    {
+                        Console.WriteLine("**********************");
+                        Console.WriteLine("Unit Testing: Category");
+                        Console.WriteLine("**********************");
+                        Console.WriteLine("Category Name Property: ");
+                        Console.WriteLine("Category Percentage Property: ");
+                        Console.WriteLine("");
+                    }
+
+                    if (i == 2)
+                    {
+                        Console.WriteLine("**********************");
+                        Console.WriteLine("Unit Testing: Assigment");
+                        Console.WriteLine("**********************");
+                        Console.WriteLine("Assigment Name Property: ");
+                        Console.WriteLine("Assigment Description Property: ");
+                        Console.WriteLine("Assigment CategoryName Property: ");
+                        Console.WriteLine("");
+                    }
+
+                    if(i < 1 || i > 3)
+                    {
+                        Console.WriteLine("Invaild Input");
+                        Console.WriteLine("Enter 1, 2 or 3");
+                        Console.WriteLine("");
+                    }
 
                 } while (i != 3);
 
