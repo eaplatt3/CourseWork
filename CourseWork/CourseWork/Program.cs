7641e690fc68260abdeb45fdb85a4afc224a91f5
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-08-27 14:44:21 -0400

    Added program with menu to test library

---
+++ CourseWork/CourseWork/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWorkLibrary { 
+
+    namespace CourseWork
+    {
+        class Program
+            {
+            static void Main(string[] args){
+
+                string num = "0";
+                int i;
+
+                do
+                {
+                    Console.WriteLine("CourseWork Testing Menu");
+                    Console.WriteLine("-----------------------");
+                    Console.WriteLine("1 - Unit Test Catgory");
+                    Console.WriteLine("2 - Unit Test Assigment");
+                    Console.WriteLine("3 - Exit");
+                    Console.Write("Enter Choice: ");
+                    num = Console.ReadLine();
+
+                    if (!Int32.TryParse(num, out i))
+                    {
+
+                    }
+                    
+
+                } while (i != 3);
+
+                if (i == 3)
+                {
+                    System.Environment.Exit(1);
+                }
+
+
+
+
+
+            }
+        }
+    }
+}
